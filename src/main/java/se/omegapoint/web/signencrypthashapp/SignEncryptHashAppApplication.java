475f7ae44d3b856ffc090e0441a9142685843b7c
per <per@erngard.com>
2017-10-13 14:01:47 +0200

    adding spring

---
+++ src/main/java/se/omegapoint/web/signencrypthashapp/SignEncryptHashAppApplication.java
@@ -0,0 +1,12 @@
+package se.omegapoint.web.signencrypthashapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignEncryptHashAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignEncryptHashAppApplication.class, args);
+	}
+}
---
+++ src/test/java/se/omegapoint/web/signencrypthashapp/SignEncryptHashAppApplicationTests.java
@@ -0,0 +1,16 @@
+package se.omegapoint.web.signencrypthashapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SignEncryptHashAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
